1f6bab6a312d09c53812981fbbeb0ec4d8f5fc48
seanwasere youtube <seanwasere@gmail.com>
2017-08-29 08:56:49 +0100

    Add files via upload

---
+++ SeansMonoRestService/Controller/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using SeansMonoRestService.Model;
+
+namespace SeansMonoRestService.Controller
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UsersController : ApiController
+    {
+
+        // GET api/Users 
+        public IEnumerable Get()
+        {
+            return Program.Users;
+        }
+
+        // GET api/Users/5 
+        public User Get(int id)
+        {
+            try
+            {
+                return Program.Users[id];
+            }
+            catch (Exception e)
+            {
+                return new User(-1, "", "");
+            }
+        }
+
+        // POST api/Users 
+        public void Post([FromBody] User value)
+        {
+            Program.Users.Add(++Program.UserCounter, new User(Program.UserCounter, value.Name, value.City));
+        }
+
+        // PUT api/Users/5 
+        public void Put(int id, [FromBody] User value)
+        {
+            Program.Users[id].Name = value.Name;
+            Program.Users[id].City = value.City;
+        }
+
+        // DELETE api/Users/5 
+        public void Delete(int id)
+        {
+            Program.Users.Remove(id);
+        }
+    }
+}
---
+++ SeansMonoRestService/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using SeansMonoRestService.Model;
+using System.Collections.Generic;
+
+namespace SeansMonoRestService
+{
+    class Program
+    {
+        public static int UserCounter = 0;
+        public static Dictionary<Int32, User> Users = new Dictionary<int, User>();
+
+        static void Main()
+        {
+            Users.Add(++UserCounter, new User(UserCounter, "Sean", "London"));
+            Users.Add(++UserCounter, new User(UserCounter, "Emmy", "Helsinki"));
+            Users.Add(++UserCounter, new User(UserCounter, "Cosmo", "New York"));
+
+            string baseAddress = "http://*:8080";
+
+            // Start OWIN host
+            using (WebApp.Start(url: baseAddress))
+            {
+                Console.WriteLine("Service Listening at " + baseAddress);
+
+                System.Threading.Thread.Sleep(-1);
+            }
+
+        }
+    }
+}
